5d0938d71a4890bd2c89539a141be2f89f0c02b8
Yash <yashgogia2001@gmail.com>
2023-08-13 02:09:54 +0530

    Not Found page updated

--- src/components/NotFound/notFound.js
+++ src/components/NotFound/notFound.js
@@ -7,15 +7,15 @@ function NotFound() {
         <div className="row">
           <div className="col-sm-12 ">
             <div className="col-sm-10 col-sm-offset-1  text-center mx-auto">
-              <div className="four_zero_four_bg">
-                <h1 className="text-center ">404</h1>
-              </div>
+              <div className="four_zero_four_bg"></div>
 
               <div className="contant_box_404">
-                <h3 className="h2">Look like you're lost</h3>
-
-                <p>the page you are looking for not avaible!</p>
-
+                <h1 className="text-center">
+                  <span className="text-danger">Oops! </span> It's a 404
+                </h1>
+                <h3 className="h2">
+                  Sorry couldn't find what your are looking for...
+                </h3>
                 <a href="/" className="link_404 no-text-decor btn-radius">
                   Go to Home
                 </a>
